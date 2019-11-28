bd0e932ca0578ef7c3c1fb9460311fffd674e889
BenLirio <benjlirio@gmail.com>
2019-11-27 20:59:56 -0500

    Clean up server dev

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,23 +1,50 @@
+/**
+ * Imports all of the packages explicitly
+ * Some are not neccessary but since I am using
+ * webpack I make them explicit
+ */
 import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../../webpack.dev.config.js'
+import http from 'http'
+import socketio from 'socket.io'
+import process from 'process'
+import console from 'console'
 
+/**
+ * Define all of the variables to be used
+ * TODO figure out a better way to use express
+ * and app to run server instead of httpServer
+ * main ones to use latter are 
+ * app
+ * io
+ * httpServer
+ */
 const app = express()
-const DIST_DIR = __dirname
+const DIST_DIR = process.env.PWD
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
 const compiler = webpack(config)
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const httpServer = http.createServer(app)
+const io = socketio(httpServer)
 
+/**
+ * This looks alot different in the prod
+ * for dev we send error if there are any
+ */
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
 }))
 
 app.use(webpackHotMiddleware(compiler))
 
+
+/**
+ * Double check that the html file actaully exists and then send it over
+ * give an error if the file does not exist.
+ */
 app.get('*', (req, res, next) => {
   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
   if (err) {
@@ -29,10 +56,20 @@ app.get('*', (req, res, next) => {
   })
 })
 
+/**
+ * All socket io functions go here
+ */
 io.on('connection', function(socket){
   console.log('a user connected with socket',socket.id)
 })
 
-http.listen(8080, function(){
-  console.log('listening on *:3000')
+
+/**
+ * Listen to port 8080 most of the time
+ * console log which port is being used
+ */
+const PORT = process.env.PORT || 8080
+httpServer.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
 })
