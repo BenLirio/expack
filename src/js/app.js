6341303bec8bb1f508809d54fbf75b607e9800d8
BenLirio <benjlirio@gmail.com>
2019-11-27 22:53:31 -0500

    Complete basic layout

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,18 @@ const socket = io()
 
 socket.emit('test', 'testing data')
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 800;
+        this.canvas.height = 600;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+  }
+  myGameArea.start()
+});
+
 export default {  }
